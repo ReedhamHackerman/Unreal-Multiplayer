a9f8de2d998508ebf93e7edc13d08ac5c57da736
ReedhamHackerman <48519104+ReedhamHackerman@users.noreply.github.com>
2021-02-24 19:58:06 -0500

    Added Extraction Zone
    Added Extraction Zone And MATERIAL For the Extraction Zone

---
+++ Source/UnrealMultiplayer/Private/FPSExtractionZone.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSExtractionZone.h"
+#include "Components/BoxComponent.h"
+#include "UnrealMultiplayer/UnrealMultiplayerCharacter.h"
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	OverlapComp->SetBoxExtent(FVector(200.0f));
+	RootComponent = OverlapComp;
+
+	OverlapComp->SetHiddenInGame(false);
+	
+	
+	
+
+}
+
+
+//void AFPSExtractionZone::BeginPlay()
+//{
+//	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
+//}
+
+//
+//void AFPSExtractionZone::HandleOverlap(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool FromSweep, const FHitResult& SweepResult)
+//{
+//	UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone!"));
+//}
+//
+
+void AFPSExtractionZone::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	AUnrealMultiplayerCharacter* myCharacter = Cast<AUnrealMultiplayerCharacter>(OtherActor);
+	if (myCharacter)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone!"));
+	}
+	
+}
+
+ 
+
